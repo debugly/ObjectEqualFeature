c1fab73aa098e4d552eabcbc21ef5d88b5691db3
许乾隆 <qianlongxu@sohu-inc.com>
2016-03-08 18:31:07 +0800

    Initial Commit

---
+++ ObjectEqualFeature/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectEqualFeature
+//
+//  Created by qianlongxu on 16/3/8.
+//  Copyright © 2016年 qianlongxu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
